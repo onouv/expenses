6a685f596524704d1380c1be76a7b92b04ae9472
olaf <onouv@t-online.de>
2024-10-31 16:37:24 +0100

    #

--- backend/expenses/src/main/resources/db/migration/V1__expenses-db.sql
+++ backend/expenses/src/main/resources/db/migration/V1__expenses-db.sql
@@ -2,10 +2,17 @@ CREATE TYPE payment_status_enum AS ENUM ('Unknown', 'Planned', 'Invoiced', 'Due'
 
 CREATE TYPE payment_type_enum AS ENUM ('Unknown', 'Cash', 'CreditCard', 'ECCard', 'BankTransfer');
 
-CREATE TABLE expense
+CREATE TABLE accounts
+(
+    account_no          VARCHAR(16) PRIMARY KEY,
+    account_name        VARCHAR(127),
+    account_description TEXT
+);
+
+CREATE TABLE expenses
 (
     expense_id              BIGINT PRIMARY KEY,
-    account_account_no      VARCHAR(16) REFERENCES account(account_no),
+    accounts_account_no      VARCHAR(16) REFERENCES accounts(account_no),
     expense_recipient       VARCHAR(120),
     expense_purpose         VARCHAR(120),
     money_micro_units       BIGINT,
@@ -16,10 +23,4 @@ CREATE TABLE expense
     expense_payment_type    VARCHAR(32),
     expense_payment_status  VARCHAR(32),
     expense_is_invoiced     BIT
-);
-CREATE TABLE account
-(
-    account_no          VARCHAR(16) PRIMARY KEY,
-    account_name        VARCHAR(127),
-    account_description TEXT
 );
