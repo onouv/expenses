9842d641dffb3201e9bb2989d3cd59b62624c774
olaf <onouv@t-online.de>
2024-11-27 16:33:53 +0100

    = FE: checkboxes in ExpenseTable

--- frontend/expenses/features/expenses/components/ExpensesListing.tsx
+++ frontend/expenses/features/expenses/components/ExpensesListing.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import ExpenseT from "@/features/accounts/types/ExpenseT";
 import { Button, Paper, Stack, Typography } from "@mui/material";
 import ExpensesTable from "@/features/expenses/components/ExpensesTable";
@@ -6,18 +6,46 @@ import Grid from "@mui/material/Grid";
 import Link from "next/link";
 import config from "@/app-config.json";
 
+const clone = (arr: number[]): number[] => {
+  const clone: number[] = [];
+  arr.forEach((num) => {
+    clone.push(num);
+  });
+
+  return clone;
+};
+
+type Selections = number[];
 type Props = {
   account: string;
   expenses: ExpenseT[];
 };
 const ExpensesListing = ({ account, expenses }: Props): ReactElement => {
+  const [selections, setSelections] = useState<Selections>([]);
+
   return (
     <Paper elevation={3}>
       <Stack spacing={2} padding={2}>
         <Typography variant="subtitle2">
           Expenses assigned to this Account
         </Typography>
-        <ExpensesTable expenses={expenses} />
+        <ExpensesTable
+          expenses={expenses}
+          selections={selections}
+          select={(id: number) => {
+            setSelections((sel) => {
+              const newSel = clone(sel);
+              newSel.push(id);
+              return newSel;
+            });
+          }}
+          unSelect={(id: number) => {
+            setSelections((sel) => {
+              sel.splice(sel.indexOf(id), 1);
+              return clone(sel);
+            });
+          }}
+        />
         <Grid container padding={3} justifyContent="left" alignItems="center">
           <Grid item>
             <Link
--- frontend/expenses/features/expenses/components/ExpensesTable.tsx
+++ frontend/expenses/features/expenses/components/ExpensesTable.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import ExpenseT from "@/features/accounts/types/ExpenseT";
 import Table from "@mui/material/Table";
 import TableHead from "@mui/material/TableHead";
@@ -6,17 +6,22 @@ import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
 import TableBody from "@mui/material/TableBody";
 import TableContainer from "@mui/material/TableContainer";
-import { Box, Typography } from "@mui/material";
+import { Box, Checkbox, FormControlLabel, Typography } from "@mui/material";
 
 type Props = {
   expenses: ExpenseT[];
+  selections: number[];
+  select: (idx: number) => void;
+  unSelect: (idx: number) => void;
 };
-const ExpensesTable = ({ expenses }: Props): ReactElement => (
+
+const ExpensesTable = (props: Props): ReactElement => (
   <Box padding={3}>
     <TableContainer>
       <Table>
         <TableHead>
           <TableRow>
+            <TableCell sx={{ borderBottom: "none" }}></TableCell>
             <TableCell sx={{ borderBottom: "none" }}>ID</TableCell>
             <TableCell sx={{ borderBottom: "none" }}>Amount</TableCell>
             <TableCell sx={{ borderBottom: "none" }}>Recipient</TableCell>
@@ -26,28 +31,43 @@ const ExpensesTable = ({ expenses }: Props): ReactElement => (
           </TableRow>
         </TableHead>
         <TableBody>
-          {expenses.map((expense: ExpenseT) => (
-            <TableRow hover key={expense.expenseId}>
-              <TableCell sx={{ borderBottom: "none" }}>
-                <Typography>{expense.expenseId}</Typography>
-              </TableCell>
-              <TableCell sx={{ borderBottom: "none" }}>
-                <Typography>{`${expense.amount.value} ${expense.amount.currency}`}</Typography>
-              </TableCell>
-              <TableCell sx={{ borderBottom: "none" }}>
-                <Typography>{expense.recipient}</Typography>
-              </TableCell>
-              <TableCell sx={{ borderBottom: "none" }}>
-                <Typography>{expense.accruedDate.toString()}</Typography>
-              </TableCell>
-              <TableCell sx={{ borderBottom: "none" }}>
-                <Typography>{expense.purpose}</Typography>
-              </TableCell>
-              <TableCell sx={{ borderBottom: "none" }}>
-                <Typography>{expense.paymentStatus}</Typography>
-              </TableCell>
-            </TableRow>
-          ))}
+          {props.expenses.map((expense: ExpenseT) => {
+            const checked = props.selections.includes(expense.expenseId);
+            return (
+              <TableRow hover key={expense.expenseId}>
+                <TableCell sx={{ borderBottom: "none" }}>
+                  <Checkbox
+                    checked={checked}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                      if (event.target.checked) {
+                        props.select(expense.expenseId);
+                      } else {
+                        props.unSelect(expense.expenseId);
+                      }
+                    }}
+                  />
+                </TableCell>
+                <TableCell sx={{ borderBottom: "none" }}>
+                  <Typography>{expense.expenseId}</Typography>
+                </TableCell>
+                <TableCell sx={{ borderBottom: "none" }}>
+                  <Typography>{`${expense.amount.value} ${expense.amount.currency}`}</Typography>
+                </TableCell>
+                <TableCell sx={{ borderBottom: "none" }}>
+                  <Typography>{expense.recipient}</Typography>
+                </TableCell>
+                <TableCell sx={{ borderBottom: "none" }}>
+                  <Typography>{expense.accruedDate.toString()}</Typography>
+                </TableCell>
+                <TableCell sx={{ borderBottom: "none" }}>
+                  <Typography>{expense.purpose}</Typography>
+                </TableCell>
+                <TableCell sx={{ borderBottom: "none" }}>
+                  <Typography>{expense.paymentStatus}</Typography>
+                </TableCell>
+              </TableRow>
+            );
+          })}
         </TableBody>
       </Table>
     </TableContainer>
